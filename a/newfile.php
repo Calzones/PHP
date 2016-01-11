7b06eff7f9b975073de5af7140b07cf172ea03ac
Fabricio Rojas <peque_arinaga@hotmail.es>
2016-01-11 12:27:40 +0000

    mañana 11/1/16

--- a/newfile.php
+++ a/newfile.php
@@ -36,14 +36,40 @@
 		private $id;
 		private $grupo;
 		private $nombre;
+		public function __construct($nif, $nombre, $cpostal, $fnac, $id, $grupo){
+			parent::__construct($nif, $nombre, $cpostal, $fnac);
+			$this->id = $id;
+			$this->grupo = $grupo;
+		}
+		public function getId(){
+			return $this->id;
+		}
+		public function getGrupo(){
+			return $this->grupo;
+		}
+		
 	}
-	
+
 	$persona = new Persona("45368556K", "Fabricio", 35116, "14/12/1993");
 	$persona1 = new Persona("45368554G", "Paco", 32121, "ayer");
-	
-	echo "DNI: " . $persona->getNif() . "<br>\n";
-	echo "Nombre: " . $persona->getNombre() . "\n";
-	echo "Codigo postal: " . $persona->getCPostal() . "\n";
-	echo "Fecha nacimiento: " . $persona->getFnac() . "\n";
-	echo "Numero perosnas: " . Persona::getPerosnas();
+	$estudiante = new Estudiante("45368554G", "Paco", 32121, "ayer", 2, 3);
+		
+	echo "<br>DNI: " . $persona->getNif() . "<br>";
+	echo "Nombre: " . $persona->getNombre() . "<br>";
+	echo "Codigo postal: " . $persona->getCPostal() . "<br>";
+	echo "Fecha nacimiento: " . $persona->getFnac() . "<br>";
+	echo "<br>";
+	echo "DNI: " . $persona1->getNif() . "<br>";
+	echo "Nombre: " . $persona1->getNombre() . "<br>";
+	echo "Codigo postal: " . $persona1->getCPostal() . "<br>";
+	echo "Fecha nacimiento: " . $persona1->getFnac() . "<br>";
+	echo "<br>";
+	echo "DNI: " . $estudiante->getNif() . "<br>";
+	echo "Nombre: " . $estudiante->getNombre() . "<br>";
+	echo "Codigo postal: " . $estudiante->getCPostal() . "<br>";
+	echo "Fecha nacimiento: " . $estudiante->getFnac() . "<br>";
+	echo "Identificacion: " . $estudiante->getId() . "<br>";
+	echo "Grupo: " . $estudiante->getGrupo() . "<br>";
+	echo "<br>";
+	echo "Numero perosnas relativo: " . Persona::getPerosnas();
 ?>
