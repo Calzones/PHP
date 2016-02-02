ab56b87e349354be3ef0fe964ae31f391d150329
Fabricio Rojas <peque_arinaga@hotmail.es>
2016-02-02 10:10:56 +0000

    Subida proyecto

--- a/Vista/logicaVista.php
+++ a/Vista/logicaVista.php
@@ -1,14 +1,15 @@
 <?php
-$diccionario = array(
-'Titulo de la Pagina'=>'POO y MVC en PHP',
-'keywords'=>'poo, mvc, php, arquitectura de software',
-'descripcion'=>'El paradigma de la programación orientada a objetos con el
-patrón arquitectónico MVC en PHP'
-);
-$template = file_get_contents('plantilla.html');
-foreach ($diccionario as $clave=>$valor) {
-   $template = str_replace('{'.$clave.'}', $valor, $template);
-}
-
-echo $template;
+	$diccionario = array (
+			'img' => 'logo.png',
+			'Titulo de la Pagina' => 'POO y MVC en PHP',
+			'keywords' => 'poo, mvc, php, arquitectura de software',
+			'descripcion' => 'El paradigma de la programación orientada a objetos con el patrón arquitectónico MVC en PHP' 
+			);
+	$template = file_get_contents ( 'plantilla.html' );
+	
+	foreach ( $diccionario as $clave => $valor ) {
+		$template = str_replace ( '{' . $clave . '}', $valor, $template );
+	}
+	
+	echo $template;
 ?>
